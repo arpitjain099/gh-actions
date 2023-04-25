488b6a20e44eb3d79bb5bac2377f3c3a3847709a
Arpit Jain <arpitjain099@gmail.com>
2023-04-25 22:14:02 +0900

    805

--- snyk-mainpage.py
+++ snyk-mainpage.py
@@ -58,7 +58,7 @@ def text_filter(text):
 _list = [
 	{
 	"type": "limited",
-	"start": 705,
+	"start": 805,
 	"url": "https://snyk.io/advisor/packages/python/popularity/limited?page=",
 	"len": 1381
 	}
