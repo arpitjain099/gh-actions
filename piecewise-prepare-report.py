bb78d88b829933a6a291f1201575742c8e15d147
Arpit Jain <arpitjain099@gmail.com>
2023-04-25 22:31:29 +0900

    csv file 3500-4000

--- piecewise-prepare-report.py
+++ piecewise-prepare-report.py
@@ -16,7 +16,7 @@ def main():
 		script_directory = os.path.dirname(os.path.abspath(__file__))
 		print("Script directory:", script_directory)
 		for iterator, entry in enumerate(data):
-			if entry["github_url"] != "" and iterator >= 3000 and iterator < 3500:
+			if entry["github_url"] != "" and iterator >= 3500 and iterator < 4000:
 				file_path = (script_directory + "/htmloutput/" +  entry["project_name"] + ".html")
 				print(entry["github_url"])
 				print("Iterator: " + str(iterator))
