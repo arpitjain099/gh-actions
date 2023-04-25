9212f48ee508301242d322073bf9520e4cf75324
Arpit Jain <arpitjain099@gmail.com>
2023-04-25 15:06:40 +0900

    ab

--- generate-yaml.py
+++ generate-yaml.py
@@ -1,5 +1,5 @@
-for i in range(0, 2350):
-    print("    - name: " + "'" + str(i * 10 + 1) + "'")
+for i in range(0, 4):
+    print("    - name: " + '"' + str(i * 10 + 1) + '"')
     print("      run: |")
-    print("        python piecewise-prepare-report.py --start " + "'" + str(i * 10 + 1) + "'")
+    print("        python piecewise-prepare-report.py --start " + '"' + str(i * 10 + 1) + '"')
     print()
