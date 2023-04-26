5a43e8b87311583b2440030bc02c54681c634e16
Arpit Jain <3242828+arpitjain099@users.noreply.github.com>
2023-04-26 12:55:33 +0900

    Update snyk-mainpage.py

--- snyk-mainpage.py
+++ snyk-mainpage.py
@@ -58,7 +58,7 @@ def text_filter(text):
 _list = [
 	{
 	"type": "limited",
-	"start": 1305,
+	"start": 324,
 	"url": "https://snyk.io/advisor/packages/python/popularity/limited?page=",
 	"len": 1381
 	}
